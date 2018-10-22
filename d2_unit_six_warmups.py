b0618a8f128d540fdee6bc7526ddf4f0b343c806
Frank <frank@iMac3.local>
2018-10-22 08:41:18 -0400

    working with list slice

---
+++ d2_unit_six_warmups.py
@@ -0,0 +1,13 @@
+student = ["Harry", "Ron", "Hermione", "Ginny", "Fred", "Neville", "Draco"]
+
+print(student[1])
+
+print(student[2])
+
+print(student[3])
+
+print(student[4])
+
+print(student[3:])
+
+print(student[:6])
