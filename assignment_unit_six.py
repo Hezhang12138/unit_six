2d42dd2f8fd6dc1bf19f2497d61a9199d1d10b5f
Frank <frank@iMac3.local>
2018-10-24 11:10:50 -0400

    almost done

--- assignment_unit_six.py
+++ assignment_unit_six.py
@@ -1,4 +1,24 @@
+number = int(input("what maximum number you want to put in?"))
+
+
+list = [2, number]
+prime = []
+
+
+def primes(number):
+    if number == 0 or number == 1:
+        return False
+    for x in list:
+        if number % x == 0:
+            return True
+        prime.append(primes)
+
+
 def main():
-    maxium_number = int(input("what maxium number you want to put in?"))
-    for x in range (2, maxium_number):
-        
+    list = [2, number]
+    prime = []
+    primes(number)
+    print(prime)
+
+
+main()
